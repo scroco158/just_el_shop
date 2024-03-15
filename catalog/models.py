0d86ceb3b77b2d163fff81687497ad4220cee5d2
scroco158 <scroco@mail.ru>
2024-03-15 17:19:34 +0300

    step 2 DZ 19_2 Добавил возможность не заполнять поля

--- catalog/models.py
+++ catalog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название продукта')
-    description = models.TextField(verbose_name='Описание продукта')
-    picture = models.ImageField(upload_to='product_picture/', verbose_name='Фото продукта')
+    description = models.TextField(verbose_name='Описание продукта', null=True)
+    picture = models.ImageField(upload_to='product_picture/', verbose_name='Фото продукта', null=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
     price = models.FloatField(verbose_name='Стоимость продукта')
-    created_at = models.DateField(verbose_name='Дата создания продукта')
-    updated_at = models.DateField(verbose_name='Дата изменения продукта')
+    created_at = models.DateField(verbose_name='Дата создания продукта', null=True)
+    updated_at = models.DateField(verbose_name='Дата изменения продукта', null=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -21,7 +21,7 @@ class Product(models.Model):
 class Category(models.Model):
 
     name = models.CharField(max_length=100, verbose_name='Название категории')
-    description = models.TextField(verbose_name='Описание категории')
+    description = models.TextField(verbose_name='Описание категории', null=True)
 
     def __str__(self):
         return f'{self.name}'
