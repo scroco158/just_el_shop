46d1a3a857b45d681b0e34fb9b1835449717f51e
scroco158 <scroco@mail.ru>
2024-03-15 13:58:09 +0300

    step 2 DZ 19_2 до задания 4 + создал модели и применил изначальную миграцию

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название продукта')
+    description = models.TextField(verbose_name='Описание продукта')
+    picture = models.ImageField(upload_to='product_picture/', verbose_name='Фото продукта')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    price = models.FloatField(verbose_name='Стоимость продукта')
+    created_at = models.DateField(verbose_name='Дата создания продукта')
+    updated_at = models.DateField(verbose_name='Дата изменения продукта')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Название категории')
+    description = models.TextField(verbose_name='Описание категории')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+
