4817fdfa0187d160c05ad6bcf49f9f967358fed8
scroco158 <scroco@mail.ru>
2024-03-28 21:05:58 +0300

    TemplateView

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,6 @@ app_name = MainConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
-    path('contacts/', contacts_cont),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
     path('one_product/<int:pk>', ProductDetailView.as_view(), name='one_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from catalog.models import Product
 
+# def contacts_cont(requests):
+#
+#     if requests.method == 'POST':
+#         name = requests.POST.get('name')
+#         phone = requests.POST.get('phone')
+#         message = requests.POST.get('message')
+#
+#         print(f'name -> {name}\n '
+#               f'phone -> {phone}\n'
+#               f'message -> {message}\n')
+#
+#     return render(requests, 'catalog/contacts.html')
 
-def contacts_cont(requests):
 
-    if requests.method == 'POST':
-        name = requests.POST.get('name')
-        phone = requests.POST.get('phone')
-        message = requests.POST.get('message')
-
-        print(f'name -> {name}\n '
-              f'phone -> {phone}\n'
-              f'message -> {message}\n')
-
-    return render(requests, 'catalog/contacts.html')
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
 
 class ProductListView(ListView):  # ----_list.html
