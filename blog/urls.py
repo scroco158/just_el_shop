8705236bf9f7b82368b3d6a7a102053c11b90172
scroco158 <scroco@mail.ru>
2024-03-29 13:10:50 +0300

    контроллер UPDATE

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create_record'),
     path('', BlogListView.as_view(), name='list'),
     path('one_record/<int:pk>', BlogDetailView.as_view(), name='one_record'),
-]
+    path('edit/<int:pk>', BlogUpdateView.as_view(), name='update_record'),
+    ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from blog.models import BlogRecord
 
 
@@ -18,3 +18,7 @@ class BlogDetailView(DetailView):
     model = BlogRecord
 
 
+class BlogUpdateView(UpdateView):
+    model = BlogRecord
+    fields = ('title', 'body', 'picture', 'published_at', 'is_published')
+    success_url = reverse_lazy('blog:list')
