755def8341c3333bdb6260ad000a2360bf6e7411
scroco158 <scroco@mail.ru>
2024-03-29 14:24:50 +0300

    установил pytils + slug

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
 from blog.models import BlogRecord
 
 
@@ -9,6 +11,12 @@ class BlogCreateView(CreateView):
     fields = ('title', 'body', 'picture')
     success_url = reverse_lazy('blog:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_rec = form.save()
+            new_rec.slug = slugify(new_rec.title)
+            new_rec.save()
+        return super().form_valid(form)
 
 class BlogListView(ListView):
     model = BlogRecord
