66a6bef1797cbb822e5b13ae6bba933c2074fe46
scroco158 <scroco@mail.ru>
2024-04-19 17:13:01 +0300

    22_2 Вход выход через кнопку

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .apps import UsersConfig
 from .views import *
@@ -7,5 +7,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
