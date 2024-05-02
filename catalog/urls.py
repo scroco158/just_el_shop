98570f4d9d421591916bc55c070ba4b0d09177a5
scroco158 <scroco@mail.ru>
2024-05-02 12:38:30 +0300

    23_2 добавил кеширование контроллера вывода одного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from .apps import MainConfig
 from .views import ProductListView, ContactsView, ProductDetailView, ProductUpdateView, ProductCreateView, \
     ProductDeleteView, VersionCreateView
@@ -8,7 +9,7 @@ app_name = MainConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('one_product/<int:pk>', ProductDetailView.as_view(), name='one_product'),
+    path('one_product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='one_product'),
     path('update_product/<int:pk>', ProductUpdateView.as_view(), name='update_product'),
     path('create_product/', ProductCreateView.as_view(), name='create_product'),
     path('delete_product/<int:pk>', ProductDeleteView.as_view(), name='delete_product'),
